b1a6ce7ea650deb24e5832c76f1396e702a3c620
Diego Almeida <diegoalmda@gmail.com>
2025-05-11 20:04:59 -0300

    create formrequest for validation

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StorePostRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->is_admin;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ];
+    }
+}
